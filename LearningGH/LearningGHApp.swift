3a91876e7134d6f8261919f1c4b8020b3ddfe147
fivetwoar <113950687+fivetwoar@users.noreply.github.com>
2022-10-02 15:11:02 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alvaro Rodriguez on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
